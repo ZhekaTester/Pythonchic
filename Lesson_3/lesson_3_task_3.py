66cc8ed8c19c3ec6a964199c1dc3e6cad59cfb67
Zhenya <tubizez82@gmail.com>
2024-12-24 13:14:42 +0900

    1 Задание

--- Lesson_3/lesson_3_task_3.py
+++ Lesson_3/lesson_3_task_3.py
@@ -2,11 +2,13 @@ import address
 import mailing
 
 
-to_address = address.Address("12345", "Москва", "Улица Пушкина", "10", "12")
-from_address = address.Address("67890", "Санкт-Петербург", "Улица Невского", "1", "5")
+to_address = address.Address("12345", "Чита", "Ул. Ленина", "10", "12")
+from_address = address.Address("67890", "Волгоград", "Ул.Бабушкина", "1", "5")
 
 mailo = mailing.Mailing(to_address, from_address, "2500", "1452")
 print(f"{mailo.track} из {mailo.from_address.index}, {mailo.from_address.city}, {mailo.from_address.street}, "
       f"{mailo.from_address.house} - {mailo.from_address.apartment} в {mailo.to_address.index}, "
       f"{mailo.to_address.city}, {mailo.to_address.street}, {mailo.to_address.house} - {mailo.to_address.apartment}."
       f" Стоимость {mailo.cost} рублей.")
+
+
