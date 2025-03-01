ac303555789d893ad337f268125c8842d533994f
Zhenya <tubizez82@gmail.com>
2025-03-01 22:30:02 +0900

    Selenium

--- Lesson_5/05_lesson_1.py
+++ Lesson_5/05_lesson_1.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
-
 driver = webdriver.Chrome()
 driver.get("http://uitestingplayground.com/dynamicid")
 
